8e856da7a7b76986971bb74116395844932ea169
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-12-27 16:50:55 +0200

    Set MainBanner

---
+++ src/Components/MainBanner/MainBanner.css.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import CityNight from '../../Assets/Images/CityNight.jpg';
+
+export const Container = styled.div`
+    background: url(${CityNight});
+    color: #ffffff;
+    height: 100vh;
+    width: 100%;
+    margin-top: 0;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50% 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  
+`
---
+++ src/Components/MainBanner/MainBanner.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./MainBanner.css";
+
+export const MainBanner = () => {
+    return (
+        <Container>
+            <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h1>
+        </Container>
+    )
+}
