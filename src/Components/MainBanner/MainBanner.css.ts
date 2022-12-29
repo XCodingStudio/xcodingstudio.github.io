15f23ecc3fc70ffffca8eaac1397fdc5c65e4595
ciganoliviu <ciganoliviudavid@gmail.com>
2022-12-29 21:06:33 +0200

    Scroll down butto

--- src/Components/MainBanner/MainBanner.css.ts
+++ src/Components/MainBanner/MainBanner.css.ts
@@ -17,7 +17,51 @@ export const Container = styled.div`
 `
 
 export const Title = styled.h1`
-  font-size: 3.2rem;
-  text-align: center;
-  letter-spacing: 0.2rem;
+    font-size: 3.2rem;
+    text-align: center;
+    letter-spacing: 0.2rem;
+`;
+
+export const Arrows = styled.svg`
+    width: 6rem;
+    height: 7rem;
+    position: absolute;
+    left: 50%;
+    margin-left: -3rem;
+    bottom: 2rem;
+
+    path {
+        stroke: #2994D1;
+        fill: transparent;
+        stroke-width: 0.1rem;
+        animation: arrow 2s infinite;
+        -webkit-animation: arrow 2s infinite;
+    }
+
+    @keyframes arrow {
+        0% { 
+          opacity:0 
+        }
+        40% {
+          opacity:1 
+        }
+        80% {
+          opacity:0 
+        }
+        100% {
+          opacity:0 
+        }
+    }
+
+    path.a1 {
+      animation-delay: -1s;
+    }
+    
+    path.a2 {
+      animation-delay: -0.5s;
+    }
+    
+    path.a3 {
+      animation-delay: 0s;
+    }
 `;
--- src/Components/MainBanner/MainBanner.tsx
+++ src/Components/MainBanner/MainBanner.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from "./MainBanner.css";
+import { Arrows, Container, Title } from "./MainBanner.css";
 import { FC } from "react";
 import { MainBannerConfigType } from "../../utils/types";
 
@@ -10,6 +10,12 @@ export const MainBanner: FC<MainBannerType> = ({ config }) => {
     return (
         <Container>
             <Title>{config.title}</Title>
+            <br/>
+            <Arrows>
+                <path className="a1" d="M0 0 L30 32 L60 0"></path>
+                <path className="a2" d="M0 20 L30 52 L60 20"></path>
+                <path className="a3" d="M0 40 L30 72 L60 40"></path>
+            </Arrows>
         </Container>
     )
 }
